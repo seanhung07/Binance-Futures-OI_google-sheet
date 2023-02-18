f5b064db1c80fd4a3d52c1316b474d82e93ae2fa
Sean Hung <35648173+seanhung07@users.noreply.github.com>
2023-02-19 00:31:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ filtered_df = df[condition1 & condition2]
 symbols = filtered_df['symbol'].values
 
 # Define a function to fetch the open interest data for a given symbol and period
-def get_open_interest_data(symbol, period):
+def get_open_interest_data(symbol, period, session):
     url = f'https://www.binance.com/futures/data/openInterestHist?symbol={symbol}&period={period}&limit=2'
     response = session.get(url)
     if response.ok:
@@ -44,7 +44,7 @@ def get_open_interest_data(symbol, period):
     else:
         return None
     
-def get_top_long_short_ratio(symbol,period):
+def get_top_long_short_ratio(symbol, period , session):
     url = f'https://www.binance.com/futures/data/topLongShortPositionRatio?symbol={symbol}&period={period}&limit=2'
     response = session.get(url)
     if response.ok:
@@ -53,14 +53,14 @@ def get_top_long_short_ratio(symbol,period):
         return None
 
 # Define a function to process the open interest data for a given symbol
-def process_symbol_data(symbol):
+def process_symbol_data(symbol, session):
     funding = df.loc[df['symbol'] == symbol, 'lastFundingRate'].values[0]
-    data_5m = get_open_interest_data(symbol, '5m')
-    data_15m = get_open_interest_data(symbol, '15m')
-    data_30m = get_open_interest_data(symbol, '30m')
-    data_1h = get_open_interest_data(symbol, '1h')
-    data_2h = get_open_interest_data(symbol, '2h')
-    long_short_data = get_top_long_short_ratio(symbol, '5m')
+    data_5m = get_open_interest_data(symbol, '5m', session)
+    data_15m = get_open_interest_data(symbol, '15m', session)
+    data_30m = get_open_interest_data(symbol, '30m', session)
+    data_1h = get_open_interest_data(symbol, '1h', session)
+    data_2h = get_open_interest_data(symbol, '2h', session)
+    long_short_data = get_top_long_short_ratio(symbol, '5m', session)
     if data_5m and len(data_5m) > 1 and data_15m and len(data_15m) > 1 and data_30m and len(data_30m) > 1 and data_1h and len(data_1h) > 1 and data_2h and len(data_2h) > 1 and long_short_data and len(long_short_data) > 1:  
         try:
             oi_change_5m = round((float(data_5m[1]['sumOpenInterest']) - float(data_5m[0]['sumOpenInterest'])) / float(data_5m[0]['sumOpenInterest']) * 100, 1)
@@ -77,15 +77,24 @@ def process_symbol_data(symbol):
             print(f'{symbol} generated an exception: {exc}')
     return None
 
-# Use a ThreadPoolExecutor to process the symbols in parallel
-while True:
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(process_symbol_data, symbol) for symbol in symbols]
-        results = [future.result() for future in futures if future.result() is not None]
 
+# Use a ThreadPoolExecutor to process the symbols in parallel
+# Use a ThreadPoolExecutor to process the symbols in parallel
+count = 0
+while True:
+    if count % 6 == 0:
+      session = requests.Session() 
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(process_symbol_data, symbol, session) for symbol in symbols]
+        results = [future.result() for future in futures if future.result() is not None]
+    session = requests.Session()
     # Concatenate the results into a single DataFrame
-    df2 = pd.concat(results, ignore_index=True)
-    df2 = df2.sort_values(by='oi_change_5m',ascending=False)
+    if results:
+        df2 = pd.concat(results, ignore_index=True)
+    else:
+        df2 = pd.DataFrame()
+    df2 = df2.sort_values(by='oi_change_5m', ascending=False)
+    print(df2)
     mysheet.clear()
     sheet_data = df2.values.tolist()
     header = df2.columns.tolist()
@@ -100,4 +109,5 @@ while True:
         else:
             cell.value = str(val)
     mysheet.update_cells(cell_list)
-    time.sleep(30)
+    count += 1
+    time.sleep(5)
